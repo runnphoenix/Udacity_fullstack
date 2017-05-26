88a7f73c6765886656f31381826631c14b0fc363
Zhang Hanying <runnphoenix@gmail.com>
2017-05-26 11:15:29 +0800

    proj_6: show list of locations

--- proj_6/js/viewModel.js
+++ proj_6/js/viewModel.js
@@ -10,6 +10,20 @@ var polygon = null;
 // over the number of places that show.
 var placeMarkers = [];
 
+
+function viewModel() {
+  this.locations = ko.observableArray( [
+        {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+        {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+        {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+        {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+        {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+        {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+    ]);
+}
+ko.applyBindings(new viewModel());
+
+
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
